83e8691d7657124bcb44008ae497ffaf53b1c7c5
errastiKat <katrin.m.errasti@opendeusto.es>
2025-10-09 18:33:00 +0200

    Dos setup (sim y real)

--- carpeta_compartida/setup_env.sh
+++ carpeta_compartida/setup_env.sh
@@ -2,14 +2,3 @@ source ~/carpeta_compartida/gallium/setup.bash
 
 export ROS_MASTER_URI=http://tiago-222c:11311
 export ROS_IP=10.68.0.130
-
-# export ROS_MASTER_URI=http://localhost:11311
-# unset ROS_IP
-
-alias launch_tiago_simulation="roslaunch tiago_moveit_config demo.launch end_effector:=pal-gripper"
-
-
-# source ~/carpeta_compartida/gallium/setup.bash
-
-# export ROS_MASTER_URI=http://10.172.209.200:11311
-# export ROS_IP=10.172.209.2
