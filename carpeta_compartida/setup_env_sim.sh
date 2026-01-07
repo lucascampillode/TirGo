305f98a065940557c3a6ba4b92cc4b434f5c3b3d
errastiKat <166405773+errastiKat@users.noreply.github.com>
2026-01-07 13:25:52 +0100

    Delete carpeta_compartida/setup_env_sim.sh

--- carpeta_compartida/setup_env_sim.sh
+++
@@ -1,8 +0,0 @@
-source ~/carpeta_compartida/gallium/setup.bash
-
-export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=localhost
-
-unset ROS_IP
-
-alias launch_tiago_simulation="roslaunch tiago_moveit_config demo.launch end_effector:=pal-gripper"
